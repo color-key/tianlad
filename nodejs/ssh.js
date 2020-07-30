94587fc7136e7d4b939d840d7a9aefb2962757e6
fay <869770665@qq.com>
2020-07-30 17:36:31 +0800

    upd

--- nodejs/ssh.js
+++ nodejs/ssh.js
@@ -1,17 +1,39 @@
-const {execSync} = require('child_process'); 
+const {spawn} = require('child_process');
 
-// const cmdStr = 'cd ./public/home && npm install --registry=https://registry.npm.taobao.org/ && npm run export-cb';
-const cmdStr = 'cd ../home && npm install && npm run export-cb';
+const cmdStr = 'cd ./public/home && npm install --registry=https://registry.npm.taobao.org/ && npm run export-cb';
+// const cmdStr = 'cd ../home && npm install && npm run export-cb';
+
+// const reBuildHome = () => {
+//   try {
+//     const res = execSync(cmdStr);
+//     console.log(res);
+//     return {success: true};
+//   } catch (error) {
+//     console.log('build error:' + error);
+//     return {success: false, error};
+//   }
+// }
 
 const reBuildHome = () => {
-  try {
-    const res = execSync(cmdStr);
-    console.log(res);
-    return {success: true};
-  } catch (error) {
-    console.log('build error:' + error);
-    return {success: false, error};
-  }
+  const reBuildHomeSpawn = spawn(cmdStr, [], {shell: true});
+  return new Promise((resolve) => {
+    reBuildHomeSpawn.stdout.on('data', (data) => {
+      console.log(`stdout: ${data}`);
+    });
+  
+    reBuildHomeSpawn.stderr.on('data', (data) => {
+      console.error(`stderr: ${data}`);
+      resolve({success: false, error: data});
+    });
+    
+    reBuildHomeSpawn.on('close', (code) => {
+      console.log(`子进程退出，退出码 ${code}`);
+      if(code !== 0){
+        resolve({success: false, error: '异常退出'});
+      }
+      resolve({success: true});
+    });
+  })
 }
 
 module.exports = {
